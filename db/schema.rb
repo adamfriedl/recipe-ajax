304a6d63271d9e6914d20f6f0fd1c751cc3898cb
Adam Friedl <ajfriedl@gmail.com>
2017-04-15 17:01:56 -0400

    Add description to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223222509) do
+ActiveRecord::Schema.define(version: 20170415210110) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170223222509) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
