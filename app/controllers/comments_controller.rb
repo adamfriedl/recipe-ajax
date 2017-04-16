5774d8c724f60c2ec1b33a43fb6808ffc01ad41b
Adam Friedl <ajfriedl@gmail.com>
2017-04-16 08:40:01 -0400

    Comment posts, now need to display response

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,18 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+  end
+
   def create
     @comment = Comment.create(comment_params)
     render json: @comment, status: 201
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
